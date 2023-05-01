16fde69869a2fb3d378ecead641d55462d92963d
duotaro <taro8815@gmail.com>
2023-05-01 19:14:37 +0900

    test

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Navbar from './navbar'
 import Footer from './footer'
+import Link from "next/link";
 
 export default function Layout({ children }) {
   return (
@@ -8,8 +9,32 @@ export default function Layout({ children }) {
       <Head>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <meta name="description" content="TechnologyとConvenienceを組み合わせた造語。​ITがもたらす便利なものを紹介します。最近はAI関連の記事が多いです。ChatGPT / Google Bard / OpenAI GPT / Replika" />
+        <meta property="og:image"  contents="https://cdn-ak.f.st-hatena.com/images/fotolife/d/duo-taro100/20230501/20230501153944.jpg"></meta>
       </Head>
       <Navbar />
+      <header className="bg-dark py-1">
+        <div className="container px-2 px-lg-5 my-2">
+          <div className="text-center text-white">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" href={`/`}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" href={`/about/`}>
+                  AI
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" href={`/blog/`}>
+                  テクノロジー
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
       <main>{children}</main>
       <Footer />
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,41 @@
-import Head from "next/head";
-import Link from "next/link";
-import { Text } from "./blog/detail/[id].js"
-import { getDatabase } from "../lib/notion";
-import Layout from '../components/layout'
-
+import Head from "next/head.js";
+import Link from "next/link.js";
+import { getDatabase } from "../lib/notion.js";
+import Layout from '../components/layout.js'
 export const databaseId = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID;
-console.log("▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲")
-console.log(databaseId)
-console.log("▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲")
+
+export const Text = ({ text }) => {
+  if (!text) {
+    return null;
+  }
+  return text.map((value) => {
+    const {
+      annotations: { bold, code, color, italic, strikethrough, underline },
+      text,
+    } = value;
+    return (
+      <span
+        className={[
+          bold ? styles.bold : "",
+          code ? styles.code : "",
+          italic ? styles.italic : "",
+          strikethrough ? styles.strikethrough : "",
+          underline ? styles.underline : "",
+        ].join(" ")}
+        style={color !== "default" ? { color } : {}}
+        key={text.content}
+      >
+        {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
+      </span>
+    );
+  });
+};
+
 export default function Home({ posts }) {
+  console.log(posts[0].properties.Src.rich_text[0].href)
+  console.log(posts[0].properties.Endpoint)
+  console.log(posts[0].properties.Dispaly)
+
   return (
     <Layout>
       <Head>
@@ -16,19 +43,23 @@ export default function Home({ posts }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="bg-dark py-1">
-            <div className="container px-2 px-lg-5 my-2">
-                <div className="text-center text-white">
-                    <h1 className="display-4 fw-bolder">all posts</h1>
-                </div>
-            </div>
-        </header>
         <section className="py-5">
             <div className="container px-4 px-lg-5 mt-5">
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                     {posts.map((post) => {
-                      const date = new Date(post.last_edited_time).toLocaleString(
-                        "en-US",
+                      if(!post.properties.Dispaly.checkbox){
+                        return
+                      }
+                      const createtDate = new Date(post.created_time).toLocaleString(
+                        "ja",
+                        {
+                          month: "short",
+                          day: "2-digit",
+                          year: "numeric",
+                        }
+                      );
+                      const lastEditDate = new Date(post.last_edited_time).toLocaleString(
+                        "ja",
                         {
                           month: "short",
                           day: "2-digit",
@@ -38,23 +69,16 @@ export default function Home({ posts }) {
                       return (
                           <div className="col mb-5" key={post.id}>
                               <div className="card h-100">
-                                  <img className="card-img-top border-bottom" src={post.properties.Image.url} alt="..." />
+                                  <img className="card-img-top border-bottom" src={post.properties.Src.rich_text[0].href} alt="..." />
                                   <div className="card-body p-4">
                                       <div className="text-center">
                                           <h5 className="fw-bolder"><Text text={post.properties.Name.title} /></h5>
-                                          <div className="d-flex justify-content-center small text-warning mb-2">
-                                            {post.properties.Tags.multi_select.map((tag) => {
-                                              return (
-                                                <Link href={`/blog/?tag=${tag.name}`} className="bi-star-fill btn btn-outline-secondary m-1"  key={tag.id}>{tag.name}</Link>
-                                              )
-                                            })}
-                                          </div>
-                                          {date}
+                                          <p>作成日 {createtDate} / 更新日 {lastEditDate}</p>
                                       </div>
                                   </div>
                                   <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
                                       <div className="text-center">
-                                        <Link className="btn btn-outline-dark mt-auto link"  href={`/blog/detail/${post.id}/`}>記事を読む</Link>
+                                        <a className="btn btn-outline-dark mt-auto link"  href={`${post.properties.Endpoint.url}/`}>記事一覧</a>
                                       </div>
                                   </div>
                               </div>
@@ -73,7 +97,7 @@ export const getStaticProps = async () => {
 
   return {
     props: {
-      posts: database,
+      posts: database
     },
     revalidate: 1,
   };
