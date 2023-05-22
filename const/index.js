65df70f845abca5ed49e01c41901fe2d895c13b9
duotaro <taro8815@gmail.com>
2023-05-22 13:15:18 +0900

    release

--- const/index.js
+++ const/index.js
@@ -3,7 +3,14 @@ export const DOMAIN = 'https://eurekayamauchi.github.io'
 /** baseurl */
 export const BASE_URL = 'https://eurekayamauchi.github.io'
 /** ジャンル */
-export const GENRES = ['ai', 'design', 'time', 'tech']
+export const GENRE_ENUM = {
+    GENRE1 : 'ai', 
+    GENRE2 : 'design', 
+    GENRE3 : 'management', 
+    GENRE4 : 'tech'
+}
+/** ジャンル */
+export const GENRES = [GENRE_ENUM.GENRE1, GENRE_ENUM.GENRE2, GENRE_ENUM.GENRE3, GENRE_ENUM.GENRE4]
 /** ジャンル一覧 メニューなどに使用 */
 export const GENRE_LIST = [
     {
@@ -15,28 +22,28 @@ export const GENRE_LIST = [
     },
     {
         name: 'AI',
-        genre: 'ai',
+        genre: GENRE_ENUM.GENRE1,
         url : `${DOMAIN}/blog/ai/list`,
         src: 'https://cdn-ak.f.st-hatena.com/images/fotolife/d/duo-taro100/20230501/20230501154023.jpg',
         icon: "bi bi-robot"
     },
     {
         name: 'デザイン',
-        genre: 'design',
+        genre: GENRE_ENUM.GENRE2,
         url : `${DOMAIN}/blog/design/list`,
         src: 'https://cdn-ak.f.st-hatena.com/images/fotolife/d/duo-taro100/20230501/20230501153952.jpg',
         icon: "bi bi-palette"
     },
     {
-        name: '時間管理',
-        genre: 'time',
-        url : `${DOMAIN}/blog/time/list`,
+        name: 'マネジメント',
+        genre: GENRE_ENUM.GENRE3,
+        url : `${DOMAIN}/blog/management/list`,
         src: 'https://cdn-ak.f.st-hatena.com/images/fotolife/d/duo-taro100/20230501/20230501154006.jpg',
         icon: "bi bi-alarm"
     },
     {
         name: 'Technology',
-        genre: 'tech',
+        genre: GENRE_ENUM.GENRE4,
         url : `${DOMAIN}/blog/tech/list`,
         src: 'https://cdn-ak.f.st-hatena.com/images/fotolife/d/duo-taro100/20230501/20230501155201.jpg',
         icon: "bi bi-cpu"
@@ -48,7 +55,7 @@ export const GENRE_LIST = [
 export const GENRE_TITLE_MAP = {
     'ai' : 'AI 人工知能 CHAT-GPT',
     'design' : 'CSS デザイン Bootstrap Tailwind',
-    'time' : '時間管理 タスク管理 プロジェクト管理 マネジメント',
+    'management' : '時間管理 タスク管理 プロジェクト管理 マネジメント',
     'tech' : '最新技術 テクノロジー Technology イノベーション',
 }
 /** 広告ID */
@@ -61,7 +68,7 @@ export const POPULAR_LIST = [
     },
     {
         title: 'シンプルなタイムトラッキングで時間とコストを削減 「Toggl Track」',
-        url: 'https://eurekayamauchi.github.io/blog/time/detail/e72dfce4-ff9e-4e3e-aa30-d0d753f2a605/'
+        url: 'https://eurekayamauchi.github.io/blog/management/detail/e72dfce4-ff9e-4e3e-aa30-d0d753f2a605/'
     },
     {
         title: '効率的かつ迅速、安全性も備えたWebAssembly (Wasm) とは？',
@@ -76,7 +83,7 @@ export const RECOMMEND_LIST = [
     },
     {
         title: 'シンプルなタイムトラッキングで時間とコストを削減 「Toggl Track」',
-        url: 'https://eurekayamauchi.github.io/blog/time/detail/e72dfce4-ff9e-4e3e-aa30-d0d753f2a605/'
+        url: 'https://eurekayamauchi.github.io/blog/management/detail/e72dfce4-ff9e-4e3e-aa30-d0d753f2a605/'
     },
     {
         title: '作業を簡単に自動化できるAI「AI Agent」がすごい',
