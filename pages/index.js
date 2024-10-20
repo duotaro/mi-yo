6ff76d1ce3f458cbbbfedd1d466053263551476b
Taro Yamauchi <yamauchi@eurekastyle.co.jp>
2024-10-20 22:16:34 +0900

    release

--- pages/index.js
+++ pages/index.js
@@ -55,14 +55,6 @@ export default function Home({ list }) {
     <Layout>
       <Head>
         <title>Misaki Yota Photos</title>
-        <meta
-          property="og:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
-        />
-        <meta
-          name="twitter:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
-        />
       </Head>
       <div className="container mx-auto max-w-[1960px] p-8">
         <GalleryProvider list={resList} tags={tags}>
